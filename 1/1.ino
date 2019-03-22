554e17ca4c1f2a37ab0e48b0b18a452b4c5bc888
THINKPAD-X220\dingc <dingcibinbin@me.com>
2019-03-22 18:38:46 +0100

    no message

--- 1/1.ino
+++ 1/1.ino
@@ -3,20 +3,62 @@
 
 const int I2C_speed = 100000    ;
 
-Sensor* sensor1;
-Sensor* sensor2;
 void setup() {
-  Serial.begin(9600);  // start serial for output
+  Serial.begin(115200);  // start serial for output
   Wire.begin();
   Wire.setClock(I2C_speed);
-  sensor1 = new Sensor(0x68);
-  sensor2 = new Sensor(0x69);
 }
 
+void printValue(Sensor& sensor)
+{
+    Serial.println("");
+    Serial.print("Device address:");
+    Serial.println(sensor.addr,HEX);
+    Serial.print("accel/g:");
+    Serial.print("x:");
+    Serial.print(sensor.accel_x_g);
+    Serial.print(",y:");
+    Serial.print(sensor.accel_y_g);
+    Serial.print(",z:");
+    Serial.print(sensor.accel_z_g);
+    Serial.println("");
+
+ 
+    Serial.print("gyro: /dps");
+    Serial.print("x:");
+    Serial.print(sensor.gyro_x_dps);
+    Serial.print(",y:");
+    Serial.print(sensor.gyro_y_dps);
+    Serial.print(",z:");
+    Serial.print(sensor.gyro_z_dps);
+    Serial.println("");
+
+    Serial.print("temp:");
+    Serial.print(sensor.temp_c);
+    Serial.println("");
+   
+    Serial.print("mag: /uT");
+    Serial.print("x:");
+    Serial.print(sensor.mag_x_tesla);
+    Serial.print(",y:");
+    Serial.print(sensor.mag_y_tesla);
+    Serial.print(",z:");
+    Serial.print(sensor.mag_z_tesla);
+    Serial.println("");
+    
+}
 
 void loop() {
- sensor1->readAll();
- sensor2->readAll();
-  delay(1000);
+  Sensor sensor1 = Sensor(0x68);
+  Sensor sensor2 = Sensor(0x69);
+  
+  while(true)
+  {
+     sensor1.readAll();
+     printValue(sensor1);
+     //sensor2.readAll();
+     delay(1000);
+
+  }
 
 }
